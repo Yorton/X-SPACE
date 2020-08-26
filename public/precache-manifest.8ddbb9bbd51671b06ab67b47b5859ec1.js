61c54ff82d5b8aa498be205718a0d311d2ca2402
unknown <cruise2955@yahoo.com.tw>
2020-08-26 15:43:48 +0800

    make it better

---
+++ public/precache-manifest.8ddbb9bbd51671b06ab67b47b5859ec1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d449775d93e4d7993cb154b195e20ca8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1b4a45f3f3782634ec1",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "efba59c50618536fd51d",
+    "url": "/static/js/2.e126675d.chunk.js"
+  },
+  {
+    "revision": "a40dfd6966cad1e061e896a0184be938",
+    "url": "/static/js/2.e126675d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1b4a45f3f3782634ec1",
+    "url": "/static/js/main.8feb6a8a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
